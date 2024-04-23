d242bfaeffd3ffb38fd002f0c239336df654b325
Mostafa Raafat <eng2mega@gmail.com>
2024-04-23 13:28:10 +0200

    Update 0-gather_data_from_an_API.py
    push update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,5 +9,5 @@
 
                  completed = [t.get("title") for t in todos if t.get("completed") is True]
                      print("Employee {} is done with tasks({}/{}):".format(
-                             user.get("name"), len(completed), len(todos)))
-                                 [print("\t {}".format(c)) for c in completed]
+                      user.get("name"), len(completed), len(todos)))
+[print("\t {}".format(c)) for c in completed]
