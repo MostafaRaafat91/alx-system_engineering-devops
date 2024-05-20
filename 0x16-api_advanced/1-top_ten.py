77cefbd3fb0eedbd8d2b725db1117b0cf616d685
MostafaRaafat91 <eng2mega@gmail.com>
2024-05-20 12:45:35 -0700

    fix topten issue

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,22 +16,18 @@ def top_ten(subreddit):
         response.raise_for_status()
         data = response.json()
         hot_posts = data['data']['children']
-        if not hot_posts:
-            print(f"No posts found for subreddit '{subreddit}'")
-        else:
+        if hot_posts:
             for post in hot_posts:
                 print(post['data']['title'])
+        else:
+            print(None)
     except requests.exceptions.HTTPError:
-        print(f"Error: Unable to fetch data for subreddit '{subreddit}'")
-    except KeyError:
-        print(f"Error: No posts found for subreddit '{subreddit}'")
-    except json.decoder.JSONDecodeError:
-        print(f"Error: Unable to decode JSON response from Reddit API for subreddit '{subreddit}'")
-    except Exception as e:
-        print(f"Error: {e}")
+        print(None)
+    except (KeyError, json.decoder.JSONDecodeError):
+        print(None)
 
 if __name__ == "__main__":
     if len(argv) < 2:
-        print("Usage: python 1-main.py <subreddit>")
+        print("Please pass an argument for the subreddit to search.")
     else:
         top_ten(argv[1])
