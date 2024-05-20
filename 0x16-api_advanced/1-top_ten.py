fc9dd6111975975482550b761a83fe3507a4d22e
MostafaRaafat91 <eng2mega@gmail.com>
2024-05-20 12:42:20 -0700

    fix topten issue

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,20 +5,33 @@
 import requests
 from sys import argv
 
-
 def top_ten(subreddit):
     '''
         returns the top ten posts for a given subreddit
     '''
     user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
-        print(None)
-
+        response = requests.get(url, headers=user)
+        response.raise_for_status()
+        data = response.json()
+        hot_posts = data['data']['children']
+        if not hot_posts:
+            print(f"No posts found for subreddit '{subreddit}'")
+        else:
+            for post in hot_posts:
+                print(post['data']['title'])
+    except requests.exceptions.HTTPError:
+        print(f"Error: Unable to fetch data for subreddit '{subreddit}'")
+    except KeyError:
+        print(f"Error: No posts found for subreddit '{subreddit}'")
+    except json.decoder.JSONDecodeError:
+        print(f"Error: Unable to decode JSON response from Reddit API for subreddit '{subreddit}'")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    top_ten(argv[1])
+    if len(argv) < 2:
+        print("Usage: python 1-main.py <subreddit>")
+    else:
+        top_ten(argv[1])
