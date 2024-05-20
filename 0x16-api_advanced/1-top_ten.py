0394579bd0e9c4fa2d56979c8bc49cb3b538ebff
MostafaRaafat91 <eng2mega@gmail.com>
2024-05-20 12:51:47 -0700

    fix topten issue

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,21 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
-from sys import argv
+
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json?limit=10'
-    try:
-        response = requests.get(url, headers=user)
-        response.raise_for_status()
-        data = response.json()
-        hot_posts = data['data']['children']
-        if hot_posts:
-            for post in hot_posts:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except requests.exceptions.HTTPError:
-        print(None)
-    except (KeyError, json.decoder.JSONDecodeError):
-        print(None)
-
-if __name__ == "__main__":
-    if len(argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(argv[1])
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16.api.advanced.project by /u/BaseFeisty2757"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
